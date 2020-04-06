bf5b884e06885616b37146f5f3aae4cd17ea0301
jatinarora <jatinarora2409@gmail.com>
2020-04-06 05:44:01 -0500

    Fixing Bug

--- cnn/denoise_dataset.py
+++ cnn/denoise_dataset.py
@@ -45,7 +45,6 @@ class DENOISE_DATASET(VisionDataset):
 
         # doing this so that it is consistent with all other datasets
         # to return a PIL Image
-        img = Image.fromarray(img)
         if self.transform is not None:
             img = self.transform(img)
 
