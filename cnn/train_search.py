979f8fdc86d61dc795c18a82b81f6fc3303228a3
jatinarora <jatinarora2409@gmail.com>
2020-04-06 06:07:22 -0500

    Fixing Bug

--- cnn/train_search.py
+++ cnn/train_search.py
@@ -164,13 +164,12 @@ def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr):
     nn.utils.clip_grad_norm(model.parameters(), args.grad_clip)
     optimizer.step()
 
-    prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+    prec1 = utils.accuracy(logits, target)
     objs.update(loss.data[0], n)
     top1.update(prec1.data[0], n)
-    top5.update(prec5.data[0], n)
 
     if step % args.report_freq == 0:
-      logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+      logging.info('train %03d %e %f', step, objs.avg, top1.avg)
 
   return top1.avg, objs.avg
 
