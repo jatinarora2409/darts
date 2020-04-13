a5c29ef8255fd5550a74c25b456b3f0631342d4a
jatinarora <jatinarora2409@gmail.com>
2020-04-13 03:25:45 -0500

    Fixing Code

--- cnn/train.py
+++ cnn/train.py
@@ -43,6 +43,8 @@ parser.add_argument('--save', type=str, default='EXP', help='experiment name')
 parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--arch', type=str, default='DARTS', help='which architecture to use')
 parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
+parser.add_argument('--train_portion', type=float, default=0.8, help='portion of training data')
+
 args = parser.parse_args()
 
 args.save = 'eval-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
