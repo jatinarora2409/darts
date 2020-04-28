60651bb1dbaf63b48179a698e1060ff7eb2edc68
GRISHMA GUPTA <60323683+Grishma31@users.noreply.github.com>
2020-04-28 14:55:09 -0500

    Huffman Encoding changes for DARTS Compression Model.

--- compression/net/huffmancoding.py
+++ compression/net/huffmancoding.py
@@ -11,6 +11,7 @@ from scipy.sparse import csr_matrix, csc_matrix
 Node = namedtuple('Node', 'freq value left right')
 Node.__lt__ = lambda x, y: x.freq < y.freq
 
+
 def huffman_encode(arr, prefix, save_dir='./'):
     """
     Encodes numpy array 'arr' and saves to `save_dir`
@@ -22,7 +23,7 @@ def huffman_encode(arr, prefix, save_dir='./'):
 
     # Calculate frequency in arr
     freq_map = defaultdict(int)
-    convert_map = {'float32':float, 'int32':int}
+    convert_map = {'float32': float, 'int32': int}
     for value in np.nditer(arr):
         value = convert_map[dtype](value)
         freq_map[value] += 1
@@ -32,7 +33,7 @@ def huffman_encode(arr, prefix, save_dir='./'):
     heapify(heap)
 
     # Merge nodes
-    while(len(heap) > 1):
+    while (len(heap) > 1):
         node1 = heappop(heap)
         node2 = heappop(heap)
         merged = Node(node1.freq + node2.freq, None, node1, node2)
@@ -58,11 +59,11 @@ def huffman_encode(arr, prefix, save_dir='./'):
 
     # Dump data
     data_encoding = ''.join(value2code[convert_map[dtype](value)] for value in np.nditer(arr))
-    datasize = dump(data_encoding, directory/f'{prefix}.bin')
+    datasize = dump(data_encoding, directory / f'{prefix}.bin')
 
     # Dump codebook (huffman tree)
     codebook_encoding = encode_huffman_tree(root, dtype)
-    treesize = dump(codebook_encoding, directory/f'{prefix}_codebook.bin')
+    treesize = dump(codebook_encoding, directory / f'{prefix}_codebook.bin')
 
     return treesize, datasize
 
@@ -74,18 +75,18 @@ def huffman_decode(directory, prefix, dtype):
     directory = Path(directory)
 
     # Read the codebook
-    codebook_encoding = load(directory/f'{prefix}_codebook.bin')
+    codebook_encoding = load(directory / f'{prefix}_codebook.bin')
     root = decode_huffman_tree(codebook_encoding, dtype)
 
     # Read the data
-    data_encoding = load(directory/f'{prefix}.bin')
+    data_encoding = load(directory / f'{prefix}.bin')
 
     # Decode
     data = []
     ptr = root
     for bit in data_encoding:
         ptr = ptr.left if bit == '0' else ptr.right
-        if ptr.value is not None: # Leaf node
+        if ptr.value is not None:  # Leaf node
             data.append(ptr.value)
             ptr = root
 
@@ -98,10 +99,11 @@ def encode_huffman_tree(root, dtype):
     """
     Encodes a huffman tree to string of '0's and '1's
     """
-    converter = {'float32':float2bitstr, 'int32':int2bitstr}
+    converter = {'float32': float2bitstr, 'int32': int2bitstr}
     code_list = []
+
     def encode_node(node):
-        if node.value is not None: # node is leaf node
+        if node.value is not None:  # node is leaf node
             code_list.append('1')
             lst = list(converter[dtype](node.value))
             code_list.extend(lst)
@@ -109,6 +111,7 @@ def encode_huffman_tree(root, dtype):
             code_list.append('0')
             encode_node(node.left)
             encode_node(node.right)
+
     encode_node(root)
     return ''.join(code_list)
 
@@ -117,14 +120,15 @@ def decode_huffman_tree(code_str, dtype):
     """
     Decodes a string of '0's and '1's and costructs a huffman tree
     """
-    converter = {'float32':bitstr2float, 'int32':bitstr2int}
+    converter = {'float32': bitstr2float, 'int32': bitstr2int}
     idx = 0
+
     def decode_node():
         nonlocal idx
         info = code_str[idx]
         idx += 1
-        if info == '1': # Leaf node
-            value = converter[dtype](code_str[idx:idx+32])
+        if info == '1':  # Leaf node
+            value = converter[dtype](code_str[idx:idx + 32])
             idx += 32
             return Node(0, value, None, None)
         else:
@@ -133,7 +137,6 @@ def decode_huffman_tree(code_str, dtype):
             return Node(0, None, left, right)
 
     return decode_node()
-
 
 
 # My own dump / load logics
@@ -152,7 +155,7 @@ def dump(code_str, filename):
     code_str = header + code_str + '0' * num_of_padding
 
     # Convert string to integers and to real bytes
-    byte_arr = bytearray(int(code_str[i:i+8], 2) for i in range(0, len(code_str), 8))
+    byte_arr = bytearray(int(code_str[i:i + 8], 2) for i in range(0, len(code_str), 8))
 
     # Dump to a file
     with open(filename, 'wb') as f:
@@ -166,35 +169,39 @@ def load(filename):
     """
     with open(filename, 'rb') as f:
         header = f.read(1)
-        rest = f.read() # bytes
+        rest = f.read()  # bytes
         code_str = ''.join(f'{byte:08b}' for byte in rest)
         offset = ord(header)
         if offset != 0:
-            code_str = code_str[:-offset] # string of '0's and '1's
+            code_str = code_str[:-offset]  # string of '0's and '1's
     return code_str
 
 
 # Helper functions for converting between bit string and (float or int)
 def float2bitstr(f):
-    four_bytes = struct.pack('>f', f) # bytes
-    return ''.join(f'{byte:08b}' for byte in four_bytes) # string of '0's and '1's
+    four_bytes = struct.pack('>f', f)  # bytes
+    return ''.join(f'{byte:08b}' for byte in four_bytes)  # string of '0's and '1's
+
 
 def bitstr2float(bitstr):
-    byte_arr = bytearray(int(bitstr[i:i+8], 2) for i in range(0, len(bitstr), 8))
+    byte_arr = bytearray(int(bitstr[i:i + 8], 2) for i in range(0, len(bitstr), 8))
     return struct.unpack('>f', byte_arr)[0]
 
+
 def int2bitstr(integer):
-    four_bytes = struct.pack('>I', integer) # bytes
-    return ''.join(f'{byte:08b}' for byte in four_bytes) # string of '0's and '1's
+    four_bytes = struct.pack('>I', integer)  # bytes
+    return ''.join(f'{byte:08b}' for byte in four_bytes)  # string of '0's and '1's
+
 
 def bitstr2int(bitstr):
-    byte_arr = bytearray(int(bitstr[i:i+8], 2) for i in range(0, len(bitstr), 8))
+    byte_arr = bytearray(int(bitstr[i:i + 8], 2) for i in range(0, len(bitstr), 8))
     return struct.unpack('>I', byte_arr)[0]
 
 
 # Functions for calculating / reconstructing index diff
 def calc_index_diff(indptr):
     return indptr[1:] - indptr[:-1]
+
 
 def reconstruct_indptr(diff):
     return np.concatenate([[0], np.cumsum(diff)])
@@ -206,27 +213,44 @@ def huffman_encode_model(model, directory='encodings/'):
     original_total = 0
     compressed_total = 0
     print(f"{'Layer':<15} | {'original':>10} {'compressed':>10} {'improvement':>11} {'percent':>7}")
-    print('-'*70)
+    print('-' * 70)
     for name, param in model.named_parameters():
         if 'mask' in name:
+            print('mask')
             continue
         if 'weight' in name:
+            # print('weight')
             weight = param.data.cpu().numpy()
             shape = weight.shape
-            form = 'csr' if shape[0] < shape[1] else 'csc'
-            mat = csr_matrix(weight) if shape[0] < shape[1] else csc_matrix(weight)
+            compressed = 0
+            original = 0
+            if len(shape) > 2:
+                for i in range(shape[0]):
+                    for j in range(shape[1]):
+                        wt = weight[i][j]
+                        form = 'csr' if wt.shape[0] < wt.shape[1] else 'csc'
+                        mat = csr_matrix(wt) if wt.shape[0] < wt.shape[1] else csc_matrix(wt)
 
-            # Encode
-            t0, d0 = huffman_encode(mat.data, name+f'_{form}_data', directory)
-            t1, d1 = huffman_encode(mat.indices, name+f'_{form}_indices', directory)
-            t2, d2 = huffman_encode(calc_index_diff(mat.indptr), name+f'_{form}_indptr', directory)
+                        # Encode
+                        t0, d0 = huffman_encode(mat.data, name + f'_{form}_data', directory)
+                        t1, d1 = huffman_encode(mat.indices, name + f'_{form}_indices', directory)
+                        t2, d2 = huffman_encode(calc_index_diff(mat.indptr), name + f'_{form}_indptr', directory)
 
-            # Print statistics
-            original = mat.data.nbytes + mat.indices.nbytes + mat.indptr.nbytes
-            compressed = t0 + t1 + t2 + d0 + d1 + d2
+                        # Print statistics
+                        original += mat.data.nbytes + mat.indices.nbytes + mat.indptr.nbytes
+                        compressed += t0 + t1 + t2 + d0 + d1 + d2
 
-            print(f"{name:<15} | {original:10} {compressed:10} {original / compressed:>10.2f}x {100 * compressed / original:>6.2f}%")
-        else: # bias
+            else:
+                weight.dump(f'{directory}/{name}')
+
+                # Print statistics
+                original = weight.nbytes
+                compressed = original
+
+            print(
+                f"{name:<15} | {original:10} {compressed:10} {original / compressed:>10.2f}x {100 * compressed / original:>6.2f}%")
+
+        else:  # bias
             # Note that we do not huffman encode bias
             bias = param.data.cpu().numpy()
             bias.dump(f'{directory}/{name}')
@@ -235,12 +259,14 @@ def huffman_encode_model(model, directory='encodings/'):
             original = bias.nbytes
             compressed = original
 
-            print(f"{name:<15} | {original:10} {compressed:10} {original / compressed:>10.2f}x {100 * compressed / original:>6.2f}%")
+            print(
+                f"{name:<15} | {original:10} {compressed:10} {original / compressed:>10.2f}x {100 * compressed / original:>6.2f}%")
         original_total += original
         compressed_total += compressed
 
-    print('-'*70)
-    print(f"{'total':15} | {original_total:>10} {compressed_total:>10} {original_total / compressed_total:>10.2f}x {100 * compressed_total / original_total:>6.2f}%")
+    print('-' * 70)
+    print(
+        f"{'total':15} | {original_total:>10} {compressed_total:>10} {original_total / compressed_total:>10.2f}x {100 * compressed_total / original_total:>6.2f}%")
 
 
 def huffman_decode_model(model, directory='encodings/'):
@@ -255,9 +281,9 @@ def huffman_decode_model(model, directory='encodings/'):
             matrix = csr_matrix if shape[0] < shape[1] else csc_matrix
 
             # Decode data
-            data = huffman_decode(directory, name+f'_{form}_data', dtype='float32')
-            indices = huffman_decode(directory, name+f'_{form}_indices', dtype='int32')
-            indptr = reconstruct_indptr(huffman_decode(directory, name+f'_{form}_indptr', dtype='int32'))
+            data = huffman_decode(directory, name + f'_{form}_data', dtype='float32')
+            indices = huffman_decode(directory, name + f'_{form}_indices', dtype='int32')
+            indptr = reconstruct_indptr(huffman_decode(directory, name + f'_{form}_indptr', dtype='int32'))
 
             # Construct matrix
             mat = matrix((data, indices, indptr), shape)
@@ -266,5 +292,5 @@ def huffman_decode_model(model, directory='encodings/'):
             param.data = torch.from_numpy(mat.toarray()).to(dev)
         else:
             dev = param.device
-            bias = np.load(directory+'/'+name)
+            bias = np.load(directory + '/' + name)
             param.data = torch.from_numpy(bias).to(dev)
