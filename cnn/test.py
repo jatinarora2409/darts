e6035da499fab3cc157e65106bfa7209cfe6bf66
GRISHMA GUPTA <60323683+Grishma31@users.noreply.github.com>
2020-04-30 18:33:44 -0500

    Changes made for pruning and test file.

--- cnn/test.py
+++ cnn/test.py
@@ -1,23 +1,24 @@
-import os
 import sys
-import glob
+
+sys.path.insert(0, '../cnn/')
+sys.path.insert(0, 'net/')
 import numpy as np
 import torch
+import genotypes
 import utils
 import logging
 import argparse
 import torch.nn as nn
-import genotypes
 import torch.utils
-import torchvision.datasets as dset
 import torch.backends.cudnn as cudnn
 
 from torch.autograd import Variable
-from model import NetworkCIFAR as Network
-from denoise_dataset import DENOISE_DATASET
+from cnn.model import NetworkCIFAR as Network
+from cnn.denoise_dataset import DENOISE_DATASET
 
 parser = argparse.ArgumentParser("cifar")
-parser.add_argument('--data', type=str, default='../data/mnt/d/SIDD_Medium_Srgb/Data', help='location of the data corpus')
+parser.add_argument('--data', type=str, default='../data/mnt/d/SIDD_Medium_Srgb/Data',
+                    help='location of the data corpus')
 parser.add_argument('--train_data', type=str, default='../data/train_data/', help='location of the train_data corpus')
 parser.add_argument('--label_data', type=str, default='../data/label_data/', help='location of the test_data corpus')
 parser.add_argument('--img_cropped_height', type=int, default=32, help='img cropped height')
@@ -27,7 +28,8 @@ parser.add_argument('--report_freq', type=float, default=50, help='report freque
 parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
 parser.add_argument('--init_channels', type=int, default=36, help='num of init channels')
 parser.add_argument('--layers', type=int, default=20, help='total number of layers')
-parser.add_argument('--model_path', type=str, default='./eval-EXP-20200415-092238/weights.pt', help='path of pretrained model')
+parser.add_argument('--model_path', type=str, default='./eval-EXP-20200415-092238/weights.pt',
+                    help='path of pretrained model')
 parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
 parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
 parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
@@ -38,82 +40,95 @@ args = parser.parse_args()
 
 log_format = '%(asctime)s %(message)s'
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-    format=log_format, datefmt='%m/%d %I:%M:%S %p')
+                    format=log_format, datefmt='%m/%d %I:%M:%S %p')
 
 CIFAR_CLASSES = 10
 
 
-def run_test(model_data):
-  if not torch.cuda.is_available():
-    logging.info('no gpu device available')
-    sys.exit(1)
+def run_test(model_data, var=1):
+    if not torch.cuda.is_available():
+        logging.info('no gpu device available')
+        sys.exit(1)
 
-  np.random.seed(args.seed)
-  torch.cuda.set_device(args.gpu)
-  cudnn.benchmark = True
-  torch.manual_seed(args.seed)
-  cudnn.enabled=True
-  torch.cuda.manual_seed(args.seed)
-  logging.info('gpu device = %d' % args.gpu)
-  logging.info("args = %s", args)
+    np.random.seed(args.seed)
+    torch.cuda.set_device(args.gpu)
+    cudnn.benchmark = True
+    torch.manual_seed(args.seed)
+    cudnn.enabled = True
+    torch.cuda.manual_seed(args.seed)
+    # logging.info('gpu device = %d' % args.gpu)
+    # logging.info("args = %s", args)
 
-  genotype = eval("genotypes.%s" % args.arch)
+    genotype = genotypes.DENOISE_GENO
 
-  model = Network(args.init_channels, 10, args.layers, args.auxiliary, genotype,output_height=args.img_cropped_height,output_width=args.img_cropped_width)
-  model = model.cuda()
-  utils.load(model, model_data)
+    model = Network(args.init_channels, 10, args.layers, args.auxiliary, genotype,
+                    output_height=args.img_cropped_height, output_width=args.img_cropped_width)
 
-  logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
+    if var == 2:
+        model = torch.load(model_data)
+        model.cuda()
+    else:
+        model = model.cuda()
+        utils.load(model, model_data)
 
-  criterion = nn.MSELoss()
-  criterion = criterion.cuda()
+    logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
 
-  _, test_transform = utils._data_trainsforms_denosining_dataset(args)
-  # utils._data_transforms_cifar10(args)
-  
-  test_data = DENOISE_DATASET(root=args.data,train_folder=args.train_data,label_folder=args.label_data,train=False, transform=test_transform,target_transform=test_transform )
-  # dset.CIFAR10(root=args.data, train=False, download=True, transform=test_transform)
+    criterion = nn.MSELoss()
+    criterion = criterion.cuda()
 
-  test_queue = torch.utils.data.DataLoader(
-      test_data, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=2)
+    _, test_transform = utils._data_trainsforms_denosining_dataset(args)
+    # utils._data_transforms_cifar10(args)
 
-  model.drop_path_prob = args.drop_path_prob
-  try:
-    test_acc, _ = infer(test_queue, model, criterion)
-    logging.info('test_acc %f', test_acc)
-  finally:
-    return model
+    test_data = DENOISE_DATASET(root=args.data, train_folder=args.train_data, label_folder=args.label_data, train=False,
+                                transform=test_transform, target_transform=test_transform)
+    # dset.CIFAR10(root=args.data, train=False, download=True, transform=test_transform)
+
+    test_queue = torch.utils.data.DataLoader(
+        test_data, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=2)
+    # print(len(test_queue))
+
+    model.drop_path_prob = args.drop_path_prob
+    try:
+        if var == 1:
+            print('no test')
+        else:
+            test_acc, _ = infer(test_queue, model, criterion)
+            logging.info('test_acc %f', test_acc)
+    # except Exception as e:
+    #     print('Failed')
+    #     print(e)
+    finally:
+        return model
+
 
 def infer(test_queue, model, criterion):
-  objs = utils.AvgrageMeter()
-  model.eval()
+    objs = utils.AvgrageMeter()
+    model.eval()
+    for step, (input, target) in enumerate(test_queue):
+        input = Variable(input, volatile=True).cuda()
+        target = Variable(target, volatile=True).cuda()
+        logits, logits_aux = model(input)
+        loss = criterion(logits, target)
 
-  for step, (input, target) in enumerate(test_queue):
-    input = Variable(input, volatile=True).cuda()
-    target = Variable(target, volatile=True).cuda()
+                # prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+                # n = input.size(0)
+                # objs.update(loss.data[0], n)
+                # top1.update(prec1.data[0], n)
+                # top5.update(prec5.data[0], n)
+        if args.auxiliary:
+            loss_aux = criterion(logits_aux, target)
+            loss += args.auxiliary_weight * loss_aux
 
-    logits, logits_aux = model(input)
-    loss = criterion(logits, target)
+        n = input.size(0)
+        objs.update(loss.data, n)
 
-    # prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-    # n = input.size(0)
-    # objs.update(loss.data[0], n)
-    # top1.update(prec1.data[0], n)
-    # top5.update(prec5.data[0], n)
-    
-    if args.auxiliary:
-      loss_aux = criterion(logits_aux, target)
-      loss += args.auxiliary_weight*loss_aux
-
-    n = input.size(0)
-    objs.update(loss.data, n)
-
-    if step % args.report_freq == 0:
-      logging.info('test %03d %e', step, objs.avg)
-
-  return objs.avg
+        if step % args.report_freq == 0:
+            logging.info('test %03d %e', step, objs.avg)
+        if step == 1350:
+            print('Done with test accuracy')
+            return objs.avg
+    return objs.avg
 
 
 if __name__ == '__main__':
-  run_test()
-
+    run_test()
--- compression/pruning.py
+++ compression/pruning.py
@@ -1,13 +1,17 @@
 import argparse
 import os
+import sys
+
+sys.path.insert(0, '../cnn/')
+sys.path.insert(0, 'net/')
 
 import torch
-import torch.optim as optim
+import torch.nn.utils.prune as prune
+from tensorflow.python.ops import nn
+from torch import nn
 
-import cnn.test as test
-import compression.util as util
-
-# from net.models import LeNet
+from cnn import test
+from cnn.model import Cell, ReLUConvBN
 
 os.makedirs('saves', exist_ok=True)
 
@@ -33,7 +37,42 @@ parser.add_argument('--log', type=str, default='log.txt',
                     help='log file name')
 parser.add_argument('--sensitivity', type=float, default=2,
                     help="sensitivity value that is multiplied to layer's std in order to get threshold value")
+parser.add_argument('--output', default='saves/model_after_pruning.ptmodel', type=str,
+                    help='path to model output')
 args = parser.parse_args()
+
+
+def prune_darts(model, pruning_percentage):
+    for modules in model.children():
+        if not isinstance(modules, nn.AdaptiveAvgPool3d):
+            for module in modules:
+                if not isinstance(module, Cell):
+                    # print(list(module.named_parameters()))
+                    prune.random_unstructured(module, name="weight", amount=pruning_percentage)
+                    # print(list(module.named_parameters()))
+                    # print("Not Cell")
+                else:
+                    # print(module)
+                    for cell_module in module.children():
+                        # print("Cell")
+                        if isinstance(cell_module, ReLUConvBN):
+                            # print("ReLUConvBN")
+                            for ReLU_List in cell_module.children():
+                                for ReLU_module in ReLU_List:
+                                    if isinstance(ReLU_module, nn.Conv2d):
+                                        # if ReLU_module.kernel_size[0] == 1:
+                                        #     continue
+                                        prune.l1_unstructured(ReLU_module, name="weight", amount=pruning_percentage)
+                        elif isinstance(cell_module, nn.ModuleList):
+                            # print("nn.ModuleList")
+                            for innerModules in cell_module.children():
+                                for seqItem in innerModules.children():
+                                    for layer in seqItem:
+                                        if isinstance(layer, nn.Conv2d):
+                                            # if layer.kernel_size[0] == 1:
+                                            #     continue
+                                            prune.l1_unstructured(layer, name="weight", amount=pruning_percentage)
+
 
 # Control Seed
 # torch.manual_seed(args.seed)
@@ -47,21 +86,29 @@ if use_cuda:
 else:
     print('Not using CUDA!!!')
 
-model = test.run_test(args.model)
+model = test.run_test(args.model, 1)
 
 # NOTE : `weight_decay` term denotes L2 regularization loss term
-optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=0.0001)
-initial_optimizer_state_dict = optimizer.state_dict()
+# optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=0.0001)
+# initial_optimizer_state_dict = optimizer.state_dict()
 
-model.prune_by_std(args.sensitivity)
-print("--- After pruning ---")
-util.print_nonzeros(model)
+# model.prune_by_std(args.sensitivity)
+
+# Pruning
+# print("--- Before pruning ---")
+var1 = 0.7
+# prune_darts(model, var1)
+
+for i2 in [0.8, 0.9]:
+    model = test.run_test(args.model, 1)
+    prune_darts(model, i2)
+    print("--- After pruning --- ")
+    print(i2)
+    torch.save(model, args.output)
+    test.run_test(args.output, 2)
 
 # Retrain
-print("--- Retraining ---")
-optimizer.load_state_dict(initial_optimizer_state_dict)  # Reset the optimizer
-torch.save(model, f"saves/model_after_retraining.ptmodel")
-test.run_test(model)
 
-print("--- After Retraining ---")
-util.print_nonzeros(model)
+# torch.save(model, args.output)
+# test.run_test(args.output, 2)
+
